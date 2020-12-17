05dadbf65f6d59ae84bb9c35d7616433506edbba
h4x3rotab <h4x3rotab@gmail.com>
2020-12-18 03:24:57 +0800

    CORS in cloudflare price worker

--- scripts/cfPriceWorker.js
+++ scripts/cfPriceWorker.js
@@ -6,9 +6,19 @@ const BTG_ID = 'bitcoin-gold';
 const USD_ID = 'usd';
 const RATE_URL = `https://api.coingecko.com/api/v3/simple/price?ids=${BTG_ID}&vs_currencies=${USD_ID}`;
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+};
+
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
-})
+  if (event.request.method === "OPTIONS") {
+    event.respondWith(handleOptions(event.request))
+  } else {
+    event.respondWith(handleRequest(event.request));
+  }
+});
 
 async function fetchData() {
   const resp = await fetch(RATE_URL, { cf: { cacheTtl: 60 } });
@@ -25,7 +35,34 @@ async function handleRequest(request) {
   const data = await fetchData();
   return new Response(JSON.stringify(data), {
     headers: {
+      ...corsHeaders,
       "content-type": "application/json;charset=UTF-8"
     }
   });
+}
+
+function handleOptions(request) {
+  let headers = request.headers;
+  if (
+    headers.get("Origin") !== null &&
+    headers.get("Access-Control-Request-Method") !== null &&
+    headers.get("Access-Control-Request-Headers") !== null
+  ){
+    let respHeaders = {
+      ...corsHeaders,
+      "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers"),
+    }
+
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  }
+  else {
+    return new Response(null, {
+      headers: {
+        ...corsHeaders,
+        Allow: "GET, HEAD, POST, OPTIONS",
+      },
+    })
+  }
 }
